430014d948559826dc581cf6d3da11e756ff5f96
Devan <deshlock010101@hotmai.com>
2022-03-07 21:37:08 -0600

    completed exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //throw new NotImplementedException();
+            var temp = word.Reverse();
+            return word.SequenceEqual(temp);
+        }
+    }
+
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
+using System.Linq;
 using System;
 using Xunit;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("palindrome", false)]
+        [InlineData("hello", false)]
+        [InlineData("racecar", true)]
+        [InlineData("lol", true)]
 
+        public void MyTest(string word, bool expected)
+        {
+            //arrange
+            var test = new PalindromeExercise.WordSmith();
+            //act
+            bool actual = test.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
